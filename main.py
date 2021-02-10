9614c0878d082c51cafbdc264c7aba64788f4696
CxxlMint <58025381+CxxlMint@users.noreply.github.com>
2021-02-10 19:43:37 +0300

    Создание основы

---
+++ main.py
@@ -0,0 +1,47 @@
+import sys
+import requests
+from PIL import Image
+from PIL.ImageQt import ImageQt
+from PyQt5 import uic
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super(MyWidget, self).__init__()
+        uic.loadUi('main.ui', self)
+        self.app = app
+        self.pushButton.clicked.connect(self.change_map)
+        self.geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+        self.current_coords = [0, 0]
+        self.current_size = 0
+        self.map_api_server = "http://static-maps.yandex.ru/1.x/"
+
+    def change_map(self):
+        try:
+            self.current_coords = [self.lineEdit.text().split(', ')[0], self.lineEdit.text().split(', ')[0]]
+            self.current_size = str(self.spinBox.value())
+            map_params = {
+                "ll": ",".join(self.current_coords),
+                "z": self.current_size,
+                "l": "map",
+            }
+            response = requests.get(self.map_api_server, params=map_params)
+            with open("map.png", "wb") as file:
+                file.write(response.content)
+            self.im_now = Image.open("map.png")
+            self.b = ImageQt(self.im_now)
+            self.pixmap = QPixmap.fromImage(self.b)
+            self.image.setPixmap(self.pixmap)
+        except Exception as e:
+            print(e)
+            self.error_label.setText('Извините вы ввели координаты неправильно')
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+
+    sys.exit(app.exec())
