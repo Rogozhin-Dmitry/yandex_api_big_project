4db6828d390ae5ee99377b8f7f346ebfa38a2af1
CxxlMint <58025381+CxxlMint@users.noreply.github.com>
2021-02-17 20:29:13 +0300

    Что то начинает работать но надо ещё подправить

--- WorldMap.py
+++ WorldMap.py
@@ -1,4 +1,5 @@
 from PyQt5.QtWidgets import *
+from math import cos
 
 
 class WorldMap(QLabel):
@@ -9,8 +10,17 @@ class WorldMap(QLabel):
     def mousePressEvent(self, event):
         x_coord = event.x()
         y_coord = event.y()
-        degree_for_pixel_x = self.parent_form.spn[0] / self.size().width()
-        degree_for_pixel_y = self.parent_form.spn[1] / self.size().height()
-        degree_x = degree_for_pixel_x * x_coord
-        degree_y = degree_for_pixel_y * y_coord
+        print(x_coord, y_coord)
+        delta = 360 / pow(2, self.parent_form.zoom + 8)
+        if x_coord > self.pixmap().width() // 2:
+            degree_x = self.parent_form.cords[0] + delta * (x_coord - self.pixmap().width() // 2)
+        else:
+            degree_x = self.parent_form.cords[0] - delta * (self.pixmap().width() // 2 - x_coord)
+        delta = 180 * cos(degree_x) / pow(2, self.parent_form.zoom + 8)
+        if y_coord > self.pixmap().height() // 2:
+            degree_y = self.parent_form.cords[1] - delta * (y_coord - self.pixmap().height() // 2)
+        else:
+            degree_y = self.parent_form.cords[1] + delta * (self.pixmap().height() // 2 - y_coord)
+        print(degree_x, degree_y)
+        print(self.parent_form.cords[0], self.parent_form.cords[0])
         self.parent_form.find_object_from_click(degree_x, degree_y)
